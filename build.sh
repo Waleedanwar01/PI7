5f838f91ae2c7a3e594939539896deb9741f32c6
Waleedanwar01 <waleeddogare@gmail.com>
2026-01-11 14:07:09 +0500

    Add Node.js installation and Tailwind build steps to build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,13 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Build Tailwind CSS
+echo "Installing Node.js dependencies and building Tailwind..."
+cd theme/static_src
+npm install
+npm run build
+cd ../..
+
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py sync_media
